44c7ff937b5a240378b42c8ae1aa9e647f4bfdbc
jamie2779 <jamie2779@naver.com>
2025-03-28 06:33:50 +0900

    feat: prisma 로깅 설정

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,38 @@
 import { PrismaClient } from '@prisma/client';
 import { container } from '@sapphire/framework';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+	log: [
+		{ level: 'query', emit: 'event' },
+		{ level: 'error', emit: 'event' },
+		{ level: 'warn', emit: 'event' },
+		{ level: 'info', emit: 'event' }
+	]
+});
 
-// 필요하다면 즉시 데이터베이스에 연결해 연결 문제를 사전에 확인할 수 있습니다.
-// await prisma.$connect();
+//쿼리 로그 (요약)
+prisma.$on('query', (e) => {
+	const operation = e.query.split(' ')[0];
+	const table = e.query.match(/from\s+`?(\w+)`?/i)?.[1] || '';
+	console.log(`🟦 [${operation}] ${table}(${e.duration}ms)`);
+});
 
-// Sapphire의 container에 PrismaClient를 첨부하여 어디서든 접근할 수 있도록 함
+//에러 로그
+prisma.$on('error', (e) => {
+	console.error('🟥 [PRISMA ERROR]');
+	console.error(`🔹 ${e.message}`);
+});
+
+//경고 로그
+prisma.$on('warn', (e) => {
+	console.warn('🟨 [PRISMA WARN]');
+	console.warn(`🔸 ${e.message}`);
+});
+
+//정보 로그
+prisma.$on('info', (e) => {
+	console.info('ℹ️ [PRISMA INFO]');
+	console.info(`🔹 ${e.message}`);
+});
+
 container.prisma = prisma;
